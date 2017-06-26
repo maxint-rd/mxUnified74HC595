33f551a1f1cd2287d1e03dae8c6fb2e8d32b859d
Maxint R&D <mm_rd@maxint.nl>
2017-06-26 16:46:17 +0200

    Added support for ATtiny85

--- examples/Blink8/Blink8.ino
+++ examples/Blink8/Blink8.ino
@@ -1,15 +1,26 @@
-//#define _MXUNIFIEDOUTPUT_DEBUG 0
 #include <mxUnified74HC595.h>
 
+#if defined(MXUNIFIED_ATTINY)
+  // ATtiny85 has no regular hardware serial. You can use TinyDebugSerial (TX=3) or software serial
+  // For more info see  http://www.ernstc.dk/arduino/tinycom.html
+  #include <SoftwareSerial.h>
+  SoftwareSerial mySerial(4, 3); // RX, TX
+  //TinyDebugSerial mySerial= TinyDebugSerial(); 
+  #define Serial mySerial
+  #define LED_BUILTIN 1
+#endif
+
 // create the 74HC595 output object
-//mxUnified74HC595 unio = mxUnified74HC595();                  // use hardware SPI pins, no cascading
+mxUnified74HC595 unio = mxUnified74HC595();                  // use hardware SPI pins, no cascading
 //mxUnified74HC595 unio = mxUnified74HC595(2);               // use hardware SPI pins, two cascaded shift-registers (slightly slower, but more pins)
 //mxUnified74HC595 unio = mxUnified74HC595(10, 11, 13);      // alternative software SPI pins: SS, MOSI, SCLK, no cascading (slow, but pin-freedom)
-mxUnified74HC595 unio = mxUnified74HC595(3, 2, 0);      // alternative software SPI pins for ESP-01: SS, MOSI, SCLK, no cascading (slow, but pin-freedom)
+//mxUnified74HC595 unio = mxUnified74HC595(3, 2, 0);      // alternative software SPI pins for ESP-01: SS, MOSI, SCLK, no cascading (slow, but pin-freedom)
 //mxUnified74HC595 unio = mxUnified74HC595(10, 11, 13, 3);   // alternative software SPI pins: SS, MOSI, SCLK, three cascaded shift-registers (slow, but pin-freedom)
+//mxUnified74HC595 unio = mxUnified74HC595(0, 1, 2);      // alternative software SPI pins for Attiny85: SS, MOSI, SCLK, no cascading (slow, but pin-freedom)
 #define NUMPINS 8
 // Hardware SPI pins:
-//   ATmega328: SS=10, MOSI=11, SCLK=12
+//   ATmega328: SS=10, MOSI=11, SCLK=13
+//   ATtiny85:  SS=0, MOSI=1, SCLK=2
 //   ESP8266:   SS=15, MOSI=13, SCLK=14
 //   Suggested pins ESP-01:   SS=3 (RX), MOSI=2, SCLK=0
 
