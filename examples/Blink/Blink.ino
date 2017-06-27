60a59ee24a542ad410011da8653f6b8ed3395784
Maxint R&D <mm_rd@maxint.nl>
2017-06-27 13:33:00 +0200

    Added support for ATtiny85

--- examples/Blink/Blink.ino
+++ examples/Blink/Blink.ino
@@ -1,16 +1,29 @@
-//#define _MXUNIFIEDOUTPUT_DEBUG 0
 #include <mxUnified74HC595.h>
 
+#if defined(__AVR_ATtiny85__)
+  // ATtiny85 has no regular hardware serial. You can use TinyDebugSerial (TX=3) or software serial
+  // see also http://www.ernstc.dk/arduino/tinycom.html
+  #include <SoftwareSerial.h>
+  SoftwareSerial mySerial(4, 3); // RX, TX
+  //TinyDebugSerial mySerial= TinyDebugSerial(); 
+  #define Serial mySerial
+  #define LED_BUILTIN 1
+#endif
+
+
+
 // create the 74HC595 output object
-//mxUnified74HC595 unio = mxUnified74HC595();                  // use hardware SPI pins, no cascading
+mxUnified74HC595 unio = mxUnified74HC595();                  // use hardware SPI pins, no cascading
 //mxUnified74HC595 unio = mxUnified74HC595(2);               // use hardware SPI pins, two cascaded shift-registers (slightly slower, but more pins)
 //mxUnified74HC595 unio = mxUnified74HC595(10, 11, 13);      // alternative software SPI pins: SS, MOSI, SCLK, no cascading (slow, but pin-freedom)
 //mxUnified74HC595 unio = mxUnified74HC595(10, 11, 13, 3);   // alternative software SPI pins: SS, MOSI, SCLK, three cascaded shift-registers (slow, but pin-freedom)
-mxUnified74HC595 unio = mxUnified74HC595(0, 2, 3);   // alternative software SPI pins for ESP-01: SS=3(RX), MOSI=2, SCLK=0, three cascaded shift-registers (slow, but pin-freedom)
+//mxUnified74HC595 unio = mxUnified74HC595(0, 2, 3);   // alternative software SPI pins for ESP-01: SS=3(RX), MOSI=2, SCLK=0, three cascaded shift-registers (slow, but pin-freedom)
+//mxUnified74HC595 unio = mxUnified74HC595(0, 1, 2);   // alternative software SPI pins for ATtiny85: SS=3(RX), MOSI=2, SCLK=0, three cascaded shift-registers (slow, but pin-freedom)
 
 // Hardware SPI pins:
 //   ATmega328: SS=10, MOSI=11, SCLK=12
 //   ESP8266:   SS=15, MOSI=13, SCLK=14
+//   ATtiny85:  SS=0, MOSI=1, SCLK=2
 
 // clock-timings on 3.3V 8MHz 328 using software SPI shiftOut: 7+21 us per SCLK tick, latch-pulse digitalWrite: 10us
 // clock-timings on 3.3V 8MHz 328 using software SPI port manipulation: 3.5+2 us per SCLK tick, latch-pulse, latch-pulse digitalWrite: 10us
@@ -36,4 +49,3 @@ void loop()
   unio.digitalWrite(4, HIGH);   // set expanded pin 3 high
   delay(500);
 }
-
